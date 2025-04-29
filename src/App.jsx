eb23d0e3ab08caf3a0f575b2fdb24a79b76d4939
Abhinandan Agarkar <agarkarabhinandan333@gmail.com>
2025-04-29 21:09:27 +0530

    typo corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navabar/navbar';
+import Navbar from './components/Navbar/navbar';
 import Hero from './components/Hero/hero';
 
 function App() {
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Button from "../Button/button";
 import Logo from "../Logo/logo";
 import Search from '../SearchBox/searchbox';
-import styles from "./Navbar.module.css";
+import styles from "./navbar.module.css";
 
 export default function Navbar ({ searchData }){
     return(
